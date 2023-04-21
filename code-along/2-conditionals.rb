d0a843dda396377c7b14fd634f7dadf790bd609e
Akshita Singh <akshita.singh1@kellogg.northwestern.edu>
2023-04-21 19:30:23 +0000

    lab 2 done

--- code-along/2-conditionals.rb
+++ code-along/2-conditionals.rb
@@ -4,6 +4,49 @@
 
 # Booleans
 
+# Booleans
+# this_is_true = true
+# this_is_false = false
+
+# my_number= 5
+# puts my_number >2
+# puts my_number >16
+
+
+# Not equal to 5
+# puts number != 5
+
+# if 3 == 2
+# puts "this is wrong!"
+#     This will not work
+
+# if 3 > 2
+#     puts "yes it is"
+# end
+
+# Password exercise
+# user_entered_password = "tacos"
+# real_password = "awesome"
+
+# if user_entered_password == real_password
+#     puts "you are in"
+# else
+#     puts "get out"
+# end
+
+
+my_score = 67
+passing = 60
+
+if my_score > passing
+    puts "you passed"
+elsif my_score == passing
+puts "just passed"
+else
+    puts "failed"
+end
+
+
 # Boolean Expressions
 
 # If Conditional Logic
