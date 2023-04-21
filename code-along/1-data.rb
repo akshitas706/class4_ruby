dfac8503d79bb2b29683cfe4d7a3d9817cce4344
Akshita Singh <akshita.singh1@kellogg.northwestern.edu>
2023-04-21 18:45:04 +0000

    strings and numbers

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -5,12 +5,26 @@
 # In Ruby, there are different *types* of data:
 
 # Numbers
+# puts 5
+# puts 3
+
 
 # Perform simple math with numbers
 
+# puts 5*3
+# puts 111789654*3574896
+# puts 5.0/3.0
+# puts 5/3
+
+
 # Strings
 
+# puts "Tacos " + "are awesome"
+puts "tacos " +223
+
 # Combine strings together
+
+puts "tacos " *3
 
 # Variables
 
