ee1275cdbdb8cc0d6d374ab15fd84df0986d5297
Akshita Singh <akshita.singh1@kellogg.northwestern.edu>
2023-04-21 20:56:59 +0000

    learnt hashes

--- code-along/4-hashes.rb
+++ code-along/4-hashes.rb
@@ -4,6 +4,26 @@
 
 # Hashes are lists of *key-value pairs*
 
+me = {
+    "name" => "ash",
+    "location" => "evanston",
+    "status" => "student"
+}
+# puts me
+
+
 # Accessing data from the hash
 
+puts me["name"]
+
 # More Complex Hashes
+
+me_advanced = {
+    "name" => {"nickname" =>"ash", "given name" => "akshita"},
+    "location" => {"city" => "evanston","state" => "illinois"},
+    "status" => "student"
+}
+
+puts me_advanced
+
+puts me_advanced["location"]
